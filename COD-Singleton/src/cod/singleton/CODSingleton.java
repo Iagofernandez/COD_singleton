f3b8fb7c05ceb8bf1df88623cb63233589d39b0f
User <User@pc>
2019-03-06 18:30:07 +0100

    COD-Singleton(mejorado)

--- COD-Singleton/src/cod/singleton/CODSingleton.java
+++ COD-Singleton/src/cod/singleton/CODSingleton.java
@@ -9,7 +9,9 @@ public class CODSingleton {
 
     
     public static void main(String[] args) {
+        //creaamos el nuevo objecto
         ClaseSingleton obx1 = ClaseSingleton.getInstance("Iago","Fernandez");
+        System.out.println(obx1.toString());
     }
     
 }
